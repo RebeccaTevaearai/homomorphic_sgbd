d81f70f75e817c0456cf45209ea82e3c857e0e1c
RebeccaTevaearai <79631500+RebeccaTevaearai@users.noreply.github.com>
2022-09-15 06:07:53 -0400

    Update server.h

--- sgbd/server.h
+++ sgbd/server.h
@@ -18,6 +18,9 @@
 #include "absl/strings/numbers.h"
 #include "absl/strings/str_format.h"
 #include "absl/types/span.h"
+#include "api/api_equality_int.h"
+#include "api/api_inequality_int.h"
+#include "api/api_sum.h"
 #include "tfhe/tfhe.h"
 #include "tfhe/tfhe_io.h"
 #include "transpiler/data/tfhe_data.h"
@@ -26,12 +29,22 @@
 #include "transpiler/examples/sgbd/utils.h"
 
 #include "transpiler/examples/sgbd/api/api_and.h"
+#include "transpiler/examples/sgbd/api/api_sum.h"
+#include "transpiler/examples/sgbd/api/api_div.h"
 #include "transpiler/examples/sgbd/api/api_equality_int.h"
 #include "transpiler/examples/sgbd/api/api_equality_char.h"
+#include "transpiler/examples/sgbd/api/api_inequality_int.h"
+#include "transpiler/examples/sgbd/api/api_superior_int.h"
+#include "transpiler/examples/sgbd/api/api_inferior_int.h"
 
 #include "transpiler/examples/sgbd/api_tfhe_and.h"
+#include "transpiler/examples/sgbd/api_tfhe_sum.h"
+#include "transpiler/examples/sgbd/api_tfhe_div.h"
 #include "transpiler/examples/sgbd/api_tfhe_equality_int.h"
 #include "transpiler/examples/sgbd/api_tfhe_equality_char.h"
+#include "transpiler/examples/sgbd/api_tfhe_inequality_int.h"
+#include "transpiler/examples/sgbd/api_tfhe_superior_int.h"
+#include "transpiler/examples/sgbd/api_tfhe_inferior_int.h"
 
 
 using namespace std;
@@ -114,7 +127,7 @@ void insert_row(string tableName, vector<Tfhe<int>>& v_int, vector<Tfhe<char>>& 
 
 // SELECT
 template<typename T>
-int select_where(vector<Tfhe<int>>& cipher_result, string tableName, string columnName, T& condition_cipher, const TFheGateBootstrappingCloudKeySet* bk) {
+int select_where(vector<Tfhe<int>>& cipher_result, string tableName, string columnName, T& condition_cipher, string op, const TFheGateBootstrappingCloudKeySet* bk) {
 
     int i = database.getTableIndex(tableName);
     
@@ -127,7 +140,15 @@ int select_where(vector<Tfhe<int>>& cipher_result, string tableName, string colu
         for (int k = 0; k < database.tables[i].c_int.size(); ++k) {
             if (database.tables[i].c_int[k].name == columnName) {
                 for (int l = 0; l < database.tables[i].c_int[k].values.size(); ++l) {
-                    XLS_CHECK_OK(equality_int(cipher_result[l], database.tables[i].c_int[k].values[l], condition_cipher, bk));
+                    if (op == "==") {
+                        XLS_CHECK_OK(equality_int(cipher_result[l], database.tables[i].c_int[k].values[l], condition_cipher, bk));
+                    } else if (op == ">") {
+                        XLS_CHECK_OK(superior_int(cipher_result[l], database.tables[i].c_int[k].values[l], condition_cipher, bk));
+                    } else if (op == "<") {
+                        XLS_CHECK_OK(inferior_int(cipher_result[l], database.tables[i].c_int[k].values[l], condition_cipher, bk));
+                    } else if (op == "!=") {
+                        XLS_CHECK_OK(inequality_int(cipher_result[l], database.tables[i].c_int[k].values[l], condition_cipher, bk));
+                    }        
                 }
                 return database.tables[i].c_int[k].values.size();
             }
@@ -152,6 +173,82 @@ int select_where(vector<Tfhe<int>>& cipher_result, string tableName, string colu
     throw invalid_argument("select_where: ...");
 }
 
+int select_distinct(vector<Tfhe<int>>& cipher_result, vector<Tfhe<int>>& cipher_tmp, string tableName, string columnName, const TFheGateBootstrappingCloudKeySet* bk) {
+    int i = database.getTableIndex(tableName);
+    
+    int j = database.tables[i].getColumnIntIndex(columnName);
+
+    int table_size = database.tables[i].c_int[j].values.size();
+
+    // TODO: check if table is empty
+
+    //XLS_CHECK_OK(equality_int(cipher_tmp[0], database.tables[i].c_int[j].values[0], database.tables[i].c_int[j].values[0], bk));
+    for (int k = 1; k < table_size; ++k) {
+        for (int l = k -1; l >= 0; --l) {
+            if (l == k) {
+                XLS_CHECK_OK(inequality_int(cipher_result[k], database.tables[i].c_int[j].values[l], database.tables[i].c_int[j].values[k], bk));
+            } else {
+                XLS_CHECK_OK(inequality_int(cipher_tmp[k], database.tables[i].c_int[j].values[l], database.tables[i].c_int[j].values[k], bk));
+                XLS_CHECK_OK(tfhe_and(cipher_result[k], cipher_tmp[k], cipher_result[k], bk));
+            }
+        }
+    }
+
+    return database.tables[i].c_int[j].values.size();
+}
+
+void sum(Tfhe<int>& cipher_result, string tableName, string columnName, const TFheGateBootstrappingCloudKeySet* bk) {
+    int i = database.getTableIndex(tableName);
+    
+    int j = database.tables[i].getColumnIntIndex(columnName);
+
+    int table_size = database.tables[i].c_int[j].values.size();
+
+    // TODO: check if table is empty
+
+    if (table_size > 1) {
+        XLS_CHECK_OK(tfhe_sum(cipher_result, database.tables[i].c_int[j].values[0], database.tables[i].c_int[j].values[1], bk));
+        
+        for (int k = 2; k < table_size; ++k) {
+            XLS_CHECK_OK(tfhe_sum(cipher_result, cipher_result, database.tables[i].c_int[j].values[k], bk));
+        }
+    } else {
+        XLS_CHECK_OK(inequality_int(cipher_result, cipher_result, cipher_result, bk));
+        XLS_CHECK_OK(tfhe_sum(cipher_result, cipher_result, database.tables[i].c_int[j].values[0], bk));
+    }
+
+}
+
+
+void avg(Tfhe<int>& cipher_r, Tfhe<int>& cipher_result, vector<Tfhe<int>>& cipher_tmp, string tableName, string columnName, const TFheGateBootstrappingCloudKeySet* bk) {
+    int i = database.getTableIndex(tableName);
+    
+    int j = database.tables[i].getColumnIntIndex(columnName);
+
+    int table_size = database.tables[i].c_int[j].values.size();
+
+    for(int k = 0; k < table_size; ++k) {
+        XLS_CHECK_OK(equality_int(cipher_tmp[k], database.tables[i].c_int[j].values[k], database.tables[i].c_int[j].values[k], bk));
+        if (k > 0) {
+            XLS_CHECK_OK(tfhe_sum(cipher_tmp[0], cipher_tmp[0], cipher_tmp[k], bk));
+        }
+    }
+
+    if (table_size > 1) {
+        XLS_CHECK_OK(tfhe_sum(cipher_result, database.tables[i].c_int[j].values[0], database.tables[i].c_int[j].values[1], bk));
+        
+        for (int k = 2; k < table_size; ++k) {
+            XLS_CHECK_OK(tfhe_sum(cipher_result, cipher_result, database.tables[i].c_int[j].values[k], bk));
+        }
+    } else {
+        XLS_CHECK_OK(inequality_int(cipher_result, cipher_result, cipher_result, bk));
+        XLS_CHECK_OK(tfhe_sum(cipher_result, cipher_result, database.tables[i].c_int[j].values[0], bk));
+    }
+
+    XLS_CHECK_OK(tfhe_div(cipher_r, cipher_result, cipher_tmp[0], bk));
+
+}
+
 
 // JOIN
 tuple<int, int> inner_join(vector<Tfhe<int>>& cipher_result, string tableName1, string tableName2, string columnName1, string columnName2, const TFheGateBootstrappingCloudKeySet* bk) {
@@ -170,7 +267,6 @@ tuple<int, int> inner_join(vector<Tfhe<int>>& cipher_result, string tableName1, 
 
     for (int i = 0; i < table_1_size; ++i) {
         for (int j = 0; j < table_2_size; ++j) {
-            //cout << "k: " << k << "\n";
             XLS_CHECK_OK(equality_int(cipher_result[k], database.tables[table_index1].c_int[column_index_1].values[i], database.tables[table_index2].c_int[column_index_2].values[j], bk));
             ++k;
         }
@@ -181,11 +277,14 @@ tuple<int, int> inner_join(vector<Tfhe<int>>& cipher_result, string tableName1, 
 }
 
 
-
 // DELETE
 
 // UPDATE
-
+/*
+void update(vector<Tfhe<int>>& cipher_tmp, ) {
+    
+}
+*/
 // SHOW TABLE
 
 #endif //SGBD_SERVER_H_
