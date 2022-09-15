c621b8d338d81eaec6e4002f1a1104e1aa389aec
RebeccaTevaearai <79631500+RebeccaTevaearai@users.noreply.github.com>
2022-09-15 06:05:00 -0400

    Add files via upload

---
+++ sgbd/api/api_div.cc
@@ -0,0 +1,6 @@
+#include "api_div.h"
+
+#pragma hls_top
+int tfhe_div(int arg1, int arg2) {
+        return arg1 / arg2;
+}
---
+++ sgbd/api/api_inequality_int.cc
@@ -0,0 +1,6 @@
+#include "api_inequality_int.h"
+
+#pragma hls_top
+int inequality_int(int arg1, int arg2) {
+        return arg1 != arg2;
+}
