2ea5a53b9a739f18149b3c004ac1434f60d60f0b
RebeccaTevaearai <79631500+RebeccaTevaearai@users.noreply.github.com>
2022-09-21 03:57:23 -0400

    Update utils.h

--- sgbd/utils.h
+++ sgbd/utils.h
@@ -13,24 +13,35 @@
 
 using namespace std;
 
+struct Query {
+    string name;
+    vector<string> tablesNames;
+    vector<Tfhe<int>> arg;
+    vector<string> op;
+};
 
 template<typename T>
 struct Column {
     string tableName;
     string name;
+    string type;
     vector<T> values;
 };
 
 struct Table {
     string name;
-    int nbColumn;
-    vector<tuple<string, string>> columnName_and_type;
     vector<Column<Tfhe<int>>> c_int;
-    vector<Column<Tfhe<char>>> c_char;
+    //int nbColumn;
+    //vector<tuple<string, string>> columnName_and_type;
+    //vector<Column<Tfhe<char>>> c_char;
+
+    int getNbRow() {
+        return c_int[0].values.size();
+    }
 
     int getColumnIndex(string columnName) {
-        for (int i = 0; i < columnName_and_type.size(); ++i) {
-            if (get<0>(columnName_and_type[i]) == columnName) {
+        for (int i = 0; i < c_int.size(); ++i) {
+            if (c_int[i].name == columnName) {
                 return i;
             }
         }
@@ -38,68 +49,20 @@ struct Table {
         throw invalid_argument("Table, getColumnIndex: no column found");
     }
 
-/*
-    int * getColumnPointer(string columnName) {
-        for (int i = 0; i < columnName_and_type.size(); ++i) {
-
-            if (get<0>(columnName_and_type[i]) == columnName) {
-
-                if (get<1>(columnName_and_type[i]) == "int") {
-                    return getColumnIntPointer(columnName);
-
-                } else if (get<1>(columnName_and_type[i]) == "char") {
-                    return getColumnCharPointer(columnName);
-                }
-            }
-        }
-
-        throw invalid_argument("Table, getColumnPointer: no column found");
-    }
-*/
-    int getColumnIntIndex(string columnName) {
-        for (int i = 0 ; i < c_int.size(); ++i) {
-            if (c_int[i].name == columnName) {
-                return i;
-            }
-        }
-
-        throw invalid_argument("Table, getColumnIntIndex: no column found");
-    }
-
-    Column<Tfhe<int>> * getColumnIntPointer(string columnName) {
-        for (int i = 0 ; i < c_int.size(); ++i) {
+    Column<Tfhe<int>> * getColumnPointer(string columnName) {
+        for (int i = 0; i < c_int.size(); ++i) {
             if (c_int[i].name == columnName) {
                 return &c_int[i];
             }
         }
 
-        throw invalid_argument("Table, getColumnIntPointer: no column found");
+        throw invalid_argument("Table, getColumnIndex: no column found");
     }
 
-    int getColumncharIndex(string columnName) {
-        for (int i = 0 ; i < c_char.size(); ++i) {
-            if (c_char[i].name == columnName) {
-                return i;
-            }
-        }
-
-        throw invalid_argument("Table, getColumncharIndex: no column found");
-    }
-/*
-    int * getColumnCharPointer(string columnName) {
-        for (int i = 0 ; i < c_char.size(); ++i) {
-            if (c_char[i].name == columnName) {
-                return &c_char[i];
-            }
-        }
-
-        throw invalid_argument("Table, getColumncharPointer: no column found");
-    }
-*/
     vector<string> getColumnName() {
         vector<string> v = {};
-        for (int i = 0; i < columnName_and_type.size(); ++i) {
-            v.push_back(get<0>(columnName_and_type[i]));
+        for (int i = 0; i < c_int.size(); ++i) {
+            v.push_back(c_int[i].name);
         }
         return v;
     }
@@ -108,20 +71,15 @@ struct Table {
 
         vector<string> row = {};
 
-        for(int i = 0; i < columnName_and_type.size(); ++i) {
-
-            if (get<1>(columnName_and_type[i]) == "int") {
-
-                int j = getColumnIntIndex(get<0>(columnName_and_type[i]));
-                auto tmp = c_int[j].values[index].Decrypt(key);
+        for(int i = 0; i < c_int.size(); ++i) {
+            auto tmp = c_int[i].values[index].Decrypt(key);
+            if (c_int[i].type == "char") {
+                char c = char(tmp);
+                string str;
+                str += c;
+                row.push_back(str);
+            } else {
                 row.push_back(to_string(tmp));
-
-            } else if (get<1>(columnName_and_type[i]) == "char") {
-
-                int j = getColumncharIndex(get<0>(columnName_and_type[i]));
-                auto tmp = c_char[j].values[index].Decrypt(key);
-                string s(1, tmp);
-                row.push_back(s);
             }
         }
 
@@ -146,6 +104,8 @@ struct Database {
 Database database = {
     {},
 };
+
+
 
 
 #endif /* _UTILS_H_ */
