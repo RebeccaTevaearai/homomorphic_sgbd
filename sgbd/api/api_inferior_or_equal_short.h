0faf0c856bfef4ff5b2debaa275e06263cf58db4
RebeccaTevaearai <79631500+RebeccaTevaearai@users.noreply.github.com>
2022-10-02 13:15:49 -0400

    Add files via upload

---
+++ sgbd/api/api_inferior_or_equal_short.h
@@ -0,0 +1,7 @@
+
+#ifndef SGBD_API_INFERIOR_OR_EQUAL_SHORT_H_
+#define SGBD_API_INFERIOR_OR_EQUAL_SHORT_H_
+
+short inferior_or_equal_short(short arg1, short arg2);
+
+#endif //SGBD_API_INFERIOR_OR_EQUAL_SHORT_H_
