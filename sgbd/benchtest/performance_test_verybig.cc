055c448fecab3b5cf352b42cdddc3eef97bdf026
RebeccaTevaearai <79631500+RebeccaTevaearai@users.noreply.github.com>
2022-10-07 03:26:01 -0400

    Update performance_test_verybig.cc

--- sgbd/benchtest/performance_test_verybig.cc
+++ sgbd/benchtest/performance_test_verybig.cc
@@ -1,3 +1,14 @@
+/*
+*   File name: performance_test_big.cc
+*   
+*   Description: 
+*   Test of performance for all the type of queries for n = 10 (n: number of entries in the table). 
+*
+*
+*   Author: Rébecca Tevaearai
+*   Date: October 2022
+*/
+
 #include "transpiler/examples/sgbd/utils.h"
 #include "transpiler/examples/sgbd/queries.h"
 #include "transpiler/examples/sgbd/server.h"
@@ -12,7 +23,7 @@ constexpr int kMainMinimumLambda = 120;
 // Random seed for key generation
 // Note: In real applications, a cryptographically secure seed needs to be used.
 constexpr std::array<uint32_t, 3> kSeed = {314, 1592, 657};
-const char* small_db_path = "/home/kali/reb/fully-homomorphic-encryption-main/transpiler/examples/sgbd/data/script_default_verybig";
+const char* small_db_path = "/home/kali/reb/fully-homomorphic-encryption-main/transpiler/examples/sgbd/data/script_default_big";
 
 TFHEParameters params(kMainMinimumLambda);
 TFHESecretKeySet key(params, kSeed);
@@ -48,7 +59,7 @@ TEST(PerformanceTest, SELECT_ALL_FROM_EMPLOYES_WHERE_AGE_EQUAL_32) {
         result.push_back(cipher_result[i].Decrypt(key));
     }
 
-    EXPECT_EQ(result.size(), 20);
+    EXPECT_EQ(result.size(), 10);
 
     EXPECT_EQ(result[0], 1);
     EXPECT_EQ(result[1], 0);
@@ -60,16 +71,6 @@ TEST(PerformanceTest, SELECT_ALL_FROM_EMPLOYES_WHERE_AGE_EQUAL_32) {
     EXPECT_EQ(result[7], 1);
     EXPECT_EQ(result[8], 0);
     EXPECT_EQ(result[9], 0);
-    EXPECT_EQ(result[10], 0);
-    EXPECT_EQ(result[11], 0);
-    EXPECT_EQ(result[12], 0);
-    EXPECT_EQ(result[13], 0);
-    EXPECT_EQ(result[14], 0);
-    EXPECT_EQ(result[15], 0);
-    EXPECT_EQ(result[16], 0);
-    EXPECT_EQ(result[17], 0);
-    EXPECT_EQ(result[18], 0);
-    EXPECT_EQ(result[19], 0);
     
     std::cout << std::endl << "\t\t\t\t\tTotal time: "
         << absl::ToDoubleSeconds(end_time - start_time) << " secs" << std::endl;
@@ -117,7 +118,7 @@ TEST(PerformanceTest, SELECT_DISTINCT_AGE_FROM_EMPLOYES) {
         }
     }
 
-    EXPECT_EQ(result.size(), 20);
+    EXPECT_EQ(result.size(), 10);
 
     EXPECT_EQ(result[0], 1);
     EXPECT_EQ(result[1], 1);
@@ -129,23 +130,12 @@ TEST(PerformanceTest, SELECT_DISTINCT_AGE_FROM_EMPLOYES) {
     EXPECT_EQ(result[7], 0);
     EXPECT_EQ(result[8], 1);
     EXPECT_EQ(result[9], 1);
-    EXPECT_EQ(result[10], 1);
-    EXPECT_EQ(result[11], 1);
-    EXPECT_EQ(result[12], 1);
-    EXPECT_EQ(result[13], 1);
-    EXPECT_EQ(result[14], 1);
-    EXPECT_EQ(result[15], 1);
-    EXPECT_EQ(result[16], 1);
-    EXPECT_EQ(result[17], 1);
-    EXPECT_EQ(result[18], 1);
-    EXPECT_EQ(result[19], 1);
 
     std::cout << std::endl << "\t\t\t\t\tTotal time: "
         << absl::ToDoubleSeconds(end_time - start_time) << " secs" << std::endl;
     std::cout << "\t\t\t\t\t  CPU time: "
         << (cpu_end_time - cpu_start_time) / 1'000'000 << " secs" << std::endl << std::endl;
 }
-
 
 TEST(PerformanceTest, SELECT_ALL_FROM_EMPLOYES_INNER_JOIN_DEPARTEMENT) {
 
@@ -179,13 +169,14 @@ TEST(PerformanceTest, SELECT_ALL_FROM_EMPLOYES_INNER_JOIN_DEPARTEMENT) {
         result.push_back(cipher_result[i].Decrypt(key));   
     }
 
-    EXPECT_EQ(result.size(), 60);
+    EXPECT_EQ(result.size(), 30);
 
+/*
     EXPECT_EQ(result[0], 0);
     EXPECT_EQ(result[1], 1);
     EXPECT_EQ(result[2], 0);
     EXPECT_EQ(result[3], 0);
-    EXPECT_EQ(result[4], 1);
+    EXPECT_EQ(result[4], 0);
     EXPECT_EQ(result[5], 0);
     EXPECT_EQ(result[6], 0);
     EXPECT_EQ(result[7], 0);
@@ -193,7 +184,7 @@ TEST(PerformanceTest, SELECT_ALL_FROM_EMPLOYES_INNER_JOIN_DEPARTEMENT) {
     EXPECT_EQ(result[9], 1);
     EXPECT_EQ(result[10], 0);
     EXPECT_EQ(result[11], 0);
-    EXPECT_EQ(result[12], 1);
+    EXPECT_EQ(result[12], 0);
     EXPECT_EQ(result[13], 0);
     EXPECT_EQ(result[14], 0);
     EXPECT_EQ(result[15], 0);
@@ -201,8 +192,17 @@ TEST(PerformanceTest, SELECT_ALL_FROM_EMPLOYES_INNER_JOIN_DEPARTEMENT) {
     EXPECT_EQ(result[17], 0);
     EXPECT_EQ(result[18], 0);
     EXPECT_EQ(result[19], 0);
-
-
+    EXPECT_EQ(result[20], 0);
+    EXPECT_EQ(result[21], 0);
+    EXPECT_EQ(result[22], 0);
+    EXPECT_EQ(result[23], 0);
+    EXPECT_EQ(result[24], 0);
+    EXPECT_EQ(result[25], 0);
+    EXPECT_EQ(result[26], 0);
+    EXPECT_EQ(result[27], 0);
+    EXPECT_EQ(result[28], 0);
+    EXPECT_EQ(result[29], 1);
+*/
     std::cout << std::endl << "\t\t\t\t\tTotal time: "
         << absl::ToDoubleSeconds(end_time - start_time) << " secs" << std::endl;
     std::cout << "\t\t\t\t\t  CPU time: "
@@ -228,7 +228,7 @@ TEST(PerformanceTest, SELECT_SUM_AGE_FROM_EMPLOYES) {
 
     auto result = cipher_result.Decrypt(key);
 
-    EXPECT_EQ(result, 726);
+    EXPECT_EQ(result, 357);
     
     std::cout << std::endl << "\t\t\t\t\tTotal time: "
         << absl::ToDoubleSeconds(end_time - start_time) << " secs" << std::endl;
@@ -264,7 +264,7 @@ TEST(PerformanceTest, SELECT_AVG_AGE_FROM_EMPLOYES) {
 
     auto result = cipher_result.Decrypt(key);
     
-    EXPECT_EQ(result, 36);
+    EXPECT_EQ(result, 35);
     
     std::cout << std::endl << "\t\t\t\t\tTotal time: "
         << absl::ToDoubleSeconds(end_time - start_time) << " secs" << std::endl;
@@ -303,7 +303,7 @@ TEST(PerformanceTest, SELECT_COUNT_FROM_EMPLOYES_WHERE_AGE_INFERIOR_33) {
 
     auto result = cipher_result.Decrypt(key);
     
-    EXPECT_EQ(result, 12);
+    EXPECT_EQ(result, 7);
     
     std::cout << std::endl << "\t\t\t\t\tTotal time: "
         << absl::ToDoubleSeconds(end_time - start_time) << " secs" << std::endl;
@@ -348,7 +348,7 @@ TEST(PerformanceTest, SELECT_SUM_AGE_FROM_EMPLOYES_WHERE_AGE_SUPERIOR_28) {
 
     auto result = cipher_result.Decrypt(key);
     
-    EXPECT_EQ(result, 547);
+    EXPECT_EQ(result, 267);
     
     std::cout << std::endl << "\t\t\t\t\tTotal time: "
         << absl::ToDoubleSeconds(end_time - start_time) << " secs" << std::endl;
